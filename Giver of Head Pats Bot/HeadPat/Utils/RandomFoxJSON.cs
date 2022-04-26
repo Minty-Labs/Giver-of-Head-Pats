02f2684be221a0da11ea5023cfad138d1704e03a
Lily <admin@mintlily.lgbt>
2022-04-26 00:29:40 -0400

    Created RandomFox JSON & HTML parser

---
+++ Giver of Head Pats Bot/HeadPat/Utils/RandomFoxJSON.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+
+namespace HeadPats.Utils; 
+
+public static class RandomFoxJson {
+    public static FoxRoot? FoxData;
+
+    public static void GetData(string data) => FoxData = JsonConvert.DeserializeObject<FoxRoot>(data);
+
+    public static string? GetImage() => FoxData?.Image?.Replace("\\", "");
+
+    public static string? GetImageNumber() => FoxData?.Link?.Split('=')[1];
+}
+
+public partial class FoxRoot {
+    [JsonProperty("image")]
+    public string? Image { get; set; }
+    [JsonProperty("link")]
+    public string? Link { get; set; }
+}
