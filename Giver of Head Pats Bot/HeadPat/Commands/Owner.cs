8aa271961c6963e9881f04441bd4e05ceb21a080
Lily <admin@mintlily.lgbt>
2022-05-06 01:18:44 -0400

    Created Owner Commands

---
+++ Giver of Head Pats Bot/HeadPat/Commands/Owner.cs
@@ -0,0 +1,126 @@
+﻿using System.ComponentModel;
+using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.Entities;
+using DSharpPlus.SlashCommands;
+using DSharpPlus.SlashCommands.Attributes;
+using Emzi0767.Utilities;
+using HeadPats.Managers;
+using HeadPats.Utils;
+using cc = DSharpPlus.CommandsNext.CommandContext;
+using ic = DSharpPlus.SlashCommands.InteractionContext;
+
+namespace HeadPats.Commands; 
+
+public class Owner : BaseCommandModule {
+    public Owner() => Logger.Loadodule("OwnerCommands");
+    
+    private string FooterText(string extra = "")
+        => $"{BuildInfo.Name} (v{BuildInfo.Version}) • {BuildInfo.BuildDate}{(string.IsNullOrWhiteSpace(extra) ? "" : $" • {extra}")}";
+
+    [Command("ForceRegisterSlashCommands")]
+    [RequireOwner]
+    public async Task RegOwner(cc c) {
+        var s = Program.Slash;
+        s?.RegisterCommands<BasicSlashCommands>();
+        s?.RegisterCommands<SlashOwner>();
+        await c.RespondAsync("Done");
+    }
+}
+
+// public class RequireUserIdAttribute : SlashCheckBaseAttribute {
+//     public ulong UserId;
+//
+//     public RequireUserIdAttribute(ulong userId) => UserId = userId;
+//
+//     public override async Task<bool> ExecuteChecksAsync(InteractionContext ctx) => ctx.User.Id == UserId;
+// }
+
+public class SlashOwner : ApplicationCommandModule {
+    public SlashOwner() => Logger.Loadodule("OwnerCommands");
+    
+    private string FooterText(string extra = "")
+        => $"{BuildInfo.Name} (v{BuildInfo.Version}) • {BuildInfo.BuildDate}{(string.IsNullOrWhiteSpace(extra) ? "" : $" • {extra}")}";
+
+    [SlashCommand("activity", "Change the bot\'s Activity")]
+    public async Task ChangeActivity(ic c,
+        [Choice("Offline", "off")]
+        [Choice("Invisible", "in")]
+        [Choice("Do not Disturb", "d")]
+        [Choice("Idle", "id")]
+        [Choice("Online", "on")]
+        [Option("User Status", "Change User Status")] string userStatus,
+        
+        [Choice("Playing", "play")]
+        [Choice("Listening", "listen")]
+        [Choice("Watching", "watch")]
+        [Choice("Streaming", "stream")]
+        [Choice("Competing", "compete")]
+        [Choice("Other Text", "other")]
+        [Option("Activity Type", "Change Activity Type")] string activityType,
+        
+        string? args = "") {
+        if (c.Member.Id != 167335587488071682) {
+            await c.CreateResponseAsync("You cannot run this command.");
+            return;
+        }
+        
+        if (string.IsNullOrWhiteSpace(args)) {
+            await c.CreateResponseAsync("Please select an argument.");
+            return;
+        }
+
+        var getStatus = userStatus switch {
+            "off" => UserStatus.Offline,
+            "in"  => UserStatus.Invisible,
+            "d"   => UserStatus.DoNotDisturb,
+            "id"  => UserStatus.Idle,
+            "on"  => UserStatus.Online,
+            _     => UserStatus.Offline
+        };
+
+        var getActivity = activityType switch {
+            "play" => ActivityType.Playing,
+            "listen" => ActivityType.ListeningTo,
+            "watch" => ActivityType.Watching,
+            "stream" => ActivityType.Streaming,
+            "compete" => ActivityType.Competing,
+            "other" => ActivityType.Custom,
+            _ => ActivityType.Playing
+        };
+
+        var arg = args.Split('%');
+        var url = string.IsNullOrWhiteSpace(args) ? "https://twitch.tv/MintyLily" : arg[1];
+        var name = string.IsNullOrWhiteSpace(args) ? BuildInfo.Config.Game : arg[0];
+        
+        await c.Client!.UpdateStatusAsync(new DiscordActivity {
+            Name = name,
+            ActivityType = getActivity,
+            StreamUrl = url
+        }, getStatus);
+
+        BuildInfo.Config.Game = name;
+        BuildInfo.Config.ActivityType = Program.GetActivityAsString(getActivity);
+        BuildInfo.Config.StreamingUrl = url;
+        Configuration.Save();
+
+        var color = userStatus switch {
+            "off" => "747F8D",
+            "in"  => "747F8D",
+            "d"   => "ED4245",
+            "id"  => "FAA81A",
+            "on"  => "3BA55D",
+            _ => "FFFFFF"
+        };
+
+        var changeToStreamColor = activityType == "stream" && userStatus is not ("off" and "in");
+        
+        var e = new DiscordEmbedBuilder();
+        e.WithTitle("Changed Status");
+        e.WithColor(Colors.HexToColor(changeToStreamColor ? "593695" : color));
+        e.WithDescription($"Game: {name}\nActivityType: {Program.GetActivityAsString(getActivity)}\n{(string.IsNullOrWhiteSpace(url) ? "" : $"Stream URL: {url}")}");
+        e.WithFooter(FooterText());
+        await c.CreateResponseAsync(e.Build());
+    }
+}
---
+++ Giver of Head Pats Bot/HeadPat/Commands/Replys.cs
@@ -0,0 +1,53 @@
+﻿using System.IO.Compression;
+using System.Text;
+using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.Entities;
+using DSharpPlus.SlashCommands;
+using HeadPats.Data;
+using HeadPats.Utils;
+using cc = DSharpPlus.CommandsNext.CommandContext;
+using ic = DSharpPlus.SlashCommands.InteractionContext;
+
+namespace HeadPats.Commands; 
+
+public class Replys : BaseCommandModule {
+    public Replys() => Logger.Loadodule("Replys");
+    
+    private string FooterText(string extra = "")
+        => $"{BuildInfo.Name} (v{BuildInfo.Version}) • {BuildInfo.BuildDate}{(string.IsNullOrWhiteSpace(extra) ? "" : $" • {extra}")}";
+
+    [Command("AddReply"), Aliases("ar"), Description("Adds an auto response for the server")]
+    [RequirePermissions(Permissions.ManageMessages)]
+    public async Task AddReply(cc c, string trigger, string response) {
+        if (string.IsNullOrWhiteSpace(trigger) || string.IsNullOrWhiteSpace(response)) {
+            await c.RespondAsync($"Incorrect format. Please use the following format: `{BuildInfo.Config.Prefix}AddReply [\"Trigger\"] [\"Response\"]`\n" +
+                           "_**Note:** Use \"Quotations\" to separate the trigger from the response._");
+            return;
+        }
+        ReplyStructure.AddValue(trigger, response);
+        await c.RespondAsync("Saved trigger!");
+    }
+    
+    [Command("AddReplyGlobal"), Aliases("arg"), Description("Adds an auto response for the server or as a global response")]
+    [RequireOwner]
+    public async Task AddReply(cc c, string trigger, string response, string isGlobal) {
+        if (string.IsNullOrWhiteSpace(trigger) || string.IsNullOrWhiteSpace(response)) {
+            await c.RespondAsync($"Incorrect format. Please use the following format: `{BuildInfo.Config.Prefix}AddReplyGlobal [\"Trigger\"] [\"Response\"] [\"IsGlobal\"]`\n" +
+                                 "_**Note:** Use \"Quotations\" to separate the trigger from the response._");
+            return;
+        }
+
+        var b = isGlobal.ToLower() switch {
+            "true" => true,
+            "t" => true,
+            "false" => false,
+            "f" => false,
+            _ => false
+        };
+        
+        ReplyStructure.AddValue(trigger, response, b);
+        await c.RespondAsync($"Saved trigger{(b ? " as a global response" : "")}!");
+    }
+}
