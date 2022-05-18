e54e3fee74fd795a8bdb787cef6cb0a4c0da7006
Lily <admin@mintlily.lgbt>
2022-05-18 17:23:35 -0400

    Help Formatter - Added embed color and footer

---
+++ Giver of Head Pats Bot/HeadPat/Managers/HelpFormatter.cs
@@ -0,0 +1,20 @@
+﻿using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Converters;
+using DSharpPlus.CommandsNext.Entities;
+using DSharpPlus.Entities;
+
+namespace HeadPats.Managers; 
+
+public class HelpFormatter : DefaultHelpFormatter {
+    public HelpFormatter(CommandContext ctx) : base(ctx) { }
+
+    public override CommandHelpMessage Build() {
+        EmbedBuilder.Color = DiscordColor.SpringGreen;
+        var f = new DiscordEmbedBuilder.EmbedFooter {
+            Text = $"{BuildInfo.Name} (v{BuildInfo.Version}) • {BuildInfo.BuildDate}"
+        };
+        EmbedBuilder.Footer = f;
+
+        return base.Build();
+    }
+}
