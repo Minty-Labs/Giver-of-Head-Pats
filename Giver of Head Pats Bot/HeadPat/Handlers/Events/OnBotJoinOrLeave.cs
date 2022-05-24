9081db688ba72b6a214ffd903e1a1cc5a432d0c1
Lily <admin@mintlily.lgbt>
2022-05-23 21:19:14 -0400

    Fix message not sending (prob too many characters)

--- Giver of Head Pats Bot/HeadPat/Handlers/Events/OnBotJoinOrLeave.cs
+++ Giver of Head Pats Bot/HeadPat/Handlers/Events/OnBotJoinOrLeave.cs
@@ -15,46 +15,54 @@ public class OnBotJoinOrLeave {
     }
 
     private async Task OnLeaveGuild(DiscordClient sender, GuildDeleteEventArgs e) {
-        var em = new DiscordEmbedBuilder();
-        em.WithColor(Colors.HexToColor("FF2525"));
-        em.WithDescription($"Left server: `{e.Guild.Name}` ({e.Guild.Id})");
-        em.AddField("Created", $"{e.Guild.CreationTimestamp:F}", true);
-        em.AddField("Joined", $"{e.Guild.JoinedAt:F}", true);
-        em.AddField("IsLarge", $"{e.Guild.IsLarge}", true);
-        em.AddField("Description", $"{e.Guild.Description}");
-        em.AddField("Owner", $"{e.Guild.Owner.Username}${e.Guild.Owner.Discriminator}");
-        var sbr = new StringBuilder();
-        foreach (var r in e.Guild.Roles)
-            sbr.AppendLine(r.Value.Name);
-        em.AddField("Roles", $"{sbr}");
-        var sbf = new StringBuilder();
-        foreach (var f in e.Guild.Features)
-            sbf.AppendLine(f);
-        em.AddField("Features", $"{sbf}");
-        em.WithThumbnail(e.Guild.IconUrl);
+        try {
+            var em = new DiscordEmbedBuilder();
+            em.WithColor(Colors.HexToColor("FF2525"));
+            em.WithDescription($"Left server: `{e.Guild.Name}` ({e.Guild.Id})");
+            em.AddField("Created", $"{e.Guild.CreationTimestamp:F}", true);
+            em.AddField("Joined", $"{e.Guild.JoinedAt:F}", true);
+            em.AddField("IsLarge", $"{e.Guild.IsLarge}", true);
+            em.AddField("Description", $"{e.Guild.Description}");
+            em.AddField("Owner", $"{e.Guild.Owner.Username}#{e.Guild.Owner.Discriminator}");
+            var sbr = new StringBuilder();
+            foreach (var r in e.Guild.Roles)
+                sbr.AppendLine(r.Value.Name);
+            em.AddField("Roles", $"{sbr.ToString()[..1023]}");
+            var sbf = new StringBuilder();
+            foreach (var f in e.Guild.Features)
+                sbf.AppendLine(f);
+            em.AddField("Features", $"{sbf.ToString()[..1023]}");
+            em.WithThumbnail(e.Guild.IconUrl);
 
-        await sender.SendMessageAsync(Program.GeneralLogChannel, em.Build());
+            await sender.SendMessageAsync(Program.GeneralLogChannel, em.Build());
+        } catch (Exception ex) {
+            Logger.SendLog(ex);
+        }
     }
 
     private async Task OnJoinGuild(DiscordClient sender, GuildCreateEventArgs e) {
-        var em = new DiscordEmbedBuilder();
-        em.WithColor(Colors.HexToColor("42E66C"));
-        em.WithDescription($"Joined server: `{e.Guild.Name}` ({e.Guild.Id})");
-        em.AddField("Created", $"{e.Guild.CreationTimestamp:F}", true);
-        em.AddField("Joined", $"{e.Guild.JoinedAt:F}", true);
-        em.AddField("IsLarge", $"{e.Guild.IsLarge}", true);
-        em.AddField("Description", $"{e.Guild.Description}");
-        em.AddField("Owner", $"{e.Guild.Owner.Username}${e.Guild.Owner.Discriminator}");
-        var sbr = new StringBuilder();
-        foreach (var r in e.Guild.Roles)
-            sbr.AppendLine(r.Value.Name);
-        em.AddField("Roles", $"{sbr}");
-        var sbf = new StringBuilder();
-        foreach (var f in e.Guild.Features)
-            sbf.AppendLine(f);
-        em.AddField("Features", $"{sbf}");
-        em.WithThumbnail(e.Guild.IconUrl);
+        try {
+            var em = new DiscordEmbedBuilder();
+            em.WithColor(Colors.HexToColor("42E66C"));
+            em.WithDescription($"Joined server: `{e.Guild.Name}` ({e.Guild.Id})");
+            em.AddField("Created", $"{e.Guild.CreationTimestamp:F}", true);
+            em.AddField("Joined", $"{e.Guild.JoinedAt:F}", true);
+            em.AddField("IsLarge", $"{e.Guild.IsLarge}", true);
+            em.AddField("Description", $"{e.Guild.Description}");
+            em.AddField("Owner", $"{e.Guild.Owner.Username}#{e.Guild.Owner.Discriminator}");
+            var sbr = new StringBuilder();
+            foreach (var r in e.Guild.Roles)
+                sbr.AppendLine(r.Value.Name);
+            em.AddField("Roles", $"{sbr.ToString()[..1023]}");
+            var sbf = new StringBuilder();
+            foreach (var f in e.Guild.Features)
+                sbf.AppendLine(f);
+            em.AddField("Features", $"{sbf.ToString()[..1023]}");
+            em.WithThumbnail(e.Guild.IconUrl);
 
-        await sender.SendMessageAsync(Program.GeneralLogChannel, em.Build());
+            await sender.SendMessageAsync(Program.GeneralLogChannel, em.Build());
+        } catch (Exception ex) {
+            Logger.SendLog(ex);
+        }
     }
 }
