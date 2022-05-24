e577513508f83ef47e5a29ac280b18c3e5d38751
Lily <admin@mintlily.lgbt>
2022-05-23 21:19:20 -0400

    Misc

--- Giver of Head Pats Bot/HeadPat/Program.cs
+++ Giver of Head Pats Bot/HeadPat/Program.cs
@@ -16,7 +16,7 @@ using Pastel;
 namespace HeadPats;
 
 public static class BuildInfo {
-    public const string Version = "4.1.0";
+    public const string Version = "4.1.1";
     public const string DSharpVer = "4.3.0-nightly-01135";
     public const string MintApiVer = "1.4.0";
     public const string Name = "Giver of Head Pats";
@@ -25,7 +25,7 @@ public static class BuildInfo {
     private static readonly DateTime ShortBuildDate = DateTime.Now;
     public static bool IsDebug = true;
 #elif !DEBUG
-    private static readonly DateTime ShortBuildDate = new(2022, 5, 21, 16, 57, 00); // (year, month, day, hour, min, sec)
+    private static readonly DateTime ShortBuildDate = new(2022, 5, 23, 21, 15, 00); // (year, month, day, hour, min, sec)
     public static bool IsDebug = false;
 #endif
     public static string BuildDateShort = $"{ShortBuildDate.Day} {GetMonth(ShortBuildDate.Month)} @ {ShortBuildDate.Hour}:{ChangeSingleNumber(ShortBuildDate.Minute)}";
