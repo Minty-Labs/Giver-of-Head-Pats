cb3589fbed0976e6c588a70f691119cb9e5f3530
Lily <admin@mintlily.lgbt>
2022-06-06 23:12:09 -0400

    fuckin null checks

--- Giver of Head Pats Bot/HeadPat/Handlers/Events/OnBotJoinOrLeave.cs
+++ Giver of Head Pats Bot/HeadPat/Handlers/Events/OnBotJoinOrLeave.cs
@@ -25,9 +25,11 @@ public class OnBotJoinOrLeave {
             em.AddField("Description", e.Guild?.Description ?? "None");
             em.AddField("Owner", $"{e.Guild?.Owner.Username}#{e.Guild?.Owner.Discriminator}");
             var sbr = new StringBuilder();
-            foreach (var r in e.Guild!.Roles)
-                sbr.AppendLine(r.Value.Name);
-            em.AddField("Roles", string.IsNullOrWhiteSpace(sbr.ToString()) ? "None" : sbr.ToString()[..1023]);
+            if (e.Guild!.Roles != null) {
+                foreach (var r in e.Guild!.Roles)
+                    sbr.AppendLine(r.Value.Name);
+                em.AddField("Roles", string.IsNullOrWhiteSpace(sbr.ToString()) ? "None" : sbr.ToString()[..1023]);
+            }
             var sbf = new StringBuilder();
             foreach (var f in e.Guild!.Features)
                 sbf.AppendLine(f);
@@ -51,9 +53,11 @@ public class OnBotJoinOrLeave {
             em.AddField("Description", e.Guild.Description ?? "None");
             em.AddField("Owner", $"{e.Guild.Owner.Username}#{e.Guild.Owner.Discriminator}");
             var sbr = new StringBuilder();
-            foreach (var r in e.Guild.Roles)
-                sbr.AppendLine(r.Value.Name);
-            em.AddField("Roles", string.IsNullOrWhiteSpace(sbr.ToString()) ? "None" : sbr.ToString()[..1023]);
+            if (e.Guild.Roles != null) {
+                foreach (var r in e.Guild.Roles)
+                    sbr.AppendLine(r.Value.Name);
+                em.AddField("Roles", string.IsNullOrWhiteSpace(sbr.ToString()) ? "None" : sbr.ToString()[..1023]);
+            }
             var sbf = new StringBuilder();
             foreach (var f in e.Guild.Features)
                 sbf.AppendLine(f);
