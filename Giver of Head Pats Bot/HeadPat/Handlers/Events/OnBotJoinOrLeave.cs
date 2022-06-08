aa8d361ea3f2ced28f451e33fefcdc15f22ddb5a
Lily <admin@mintlily.lgbt>
2022-06-08 12:53:52 -0400

    How many more times am i gonna have to fix this?

--- Giver of Head Pats Bot/HeadPat/Handlers/Events/OnBotJoinOrLeave.cs
+++ Giver of Head Pats Bot/HeadPat/Handlers/Events/OnBotJoinOrLeave.cs
@@ -21,19 +21,9 @@ public class OnBotJoinOrLeave {
             em.WithDescription($"Left server: `{e.Guild?.Name}` ({e.Guild?.Id})");
             try { em.AddField("Created", $"{e.Guild?.CreationTimestamp:F}", true); } catch { em.AddField("Joined", "uknown", true); }
             try { em.AddField("Joined", $"{e.Guild?.JoinedAt:F}", true); } catch { em.AddField("Joined", "uknown", true); }
-            em.AddField("IsLarge", e.Guild?.IsLarge.ToString(), true);
+            em.AddField("Members", e.Guild?.MemberCount.ToString(), true);
             em.AddField("Description", e.Guild?.Description ?? "None");
             em.AddField("Owner", $"{e.Guild?.Owner.Username}#{e.Guild?.Owner.Discriminator}");
-            var sbr = new StringBuilder();
-            if (e.Guild!.Roles != null) {
-                foreach (var r in e.Guild!.Roles)
-                    sbr.AppendLine(r.Value.Name);
-                em.AddField("Roles", string.IsNullOrWhiteSpace(sbr.ToString()) ? "None" : sbr.ToString()[..1023]);
-            }
-            var sbf = new StringBuilder();
-            foreach (var f in e.Guild!.Features)
-                sbf.AppendLine(f);
-            em.AddField("Features", string.IsNullOrWhiteSpace(sbf.ToString()) ? "None" : sbf.ToString()[..1023]);
             em.WithThumbnail(e.Guild?.IconUrl ?? "https://totallywholeso.me/assets/img/team/null.jpg");
 
             await sender.SendMessageAsync(Program.GeneralLogChannel, em.Build());
@@ -49,19 +39,9 @@ public class OnBotJoinOrLeave {
             em.WithDescription($"Joined server: `{e.Guild.Name}` ({e.Guild.Id})");
             try { em.AddField("Created", $"{e.Guild.CreationTimestamp:F}", true); } catch { em.AddField("Joined", "uknown", true); }
             try { em.AddField("Joined", $"{e.Guild.JoinedAt:F}", true); } catch { em.AddField("Joined", "uknown", true); }
-            em.AddField("IsLarge", e.Guild.IsLarge.ToString(), true);
+            em.AddField("Members", e.Guild.MemberCount.ToString(), true);
             em.AddField("Description", e.Guild.Description ?? "None");
             em.AddField("Owner", $"{e.Guild.Owner.Username}#{e.Guild.Owner.Discriminator}");
-            var sbr = new StringBuilder();
-            if (e.Guild.Roles != null) {
-                foreach (var r in e.Guild.Roles)
-                    sbr.AppendLine(r.Value.Name);
-                em.AddField("Roles", string.IsNullOrWhiteSpace(sbr.ToString()) ? "None" : sbr.ToString()[..1023]);
-            }
-            var sbf = new StringBuilder();
-            foreach (var f in e.Guild.Features)
-                sbf.AppendLine(f);
-            em.AddField("Features", string.IsNullOrWhiteSpace(sbf.ToString()) ? "None" : sbf.ToString()[..1023]);
             em.WithThumbnail(e.Guild.IconUrl ?? "https://totallywholeso.me/assets/img/team/null.jpg");
 
             await sender.SendMessageAsync(Program.GeneralLogChannel, em.Build());
