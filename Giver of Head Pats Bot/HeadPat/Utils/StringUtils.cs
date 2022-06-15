9f2505b57bb3f015f6580ff7c90ff27ba572b1f0
Lily <admin@mintlily.lgbt>
2022-06-14 21:41:10 -0400

    Created TimeConverter & StringUtils

---
+++ Giver of Head Pats Bot/HeadPat/Utils/StringUtils.cs
@@ -0,0 +1,6 @@
+﻿namespace HeadPats.Utils; 
+
+public static class StringUtils {
+    public static string ReplaceAll(this string theStringToBeEdited, string oldCharacters, string newCharacters) // Idea from Java String.ReplaceAll()
+        => oldCharacters.ToCharArray().Aggregate(theStringToBeEdited, (current, c) => current.Replace($"{c}", newCharacters));
+}
---
+++ Giver of Head Pats Bot/HeadPat/Utils/TimeConverter.cs
@@ -0,0 +1,5 @@
+﻿namespace HeadPats.Utils; 
+
+public static class TimeConverter {
+    public static Int32 GetUnixTime(DateTime time) => (Int32)time.Subtract(new DateTime(1969, 12, 31, 20, 00, 00)).TotalSeconds;
+}
