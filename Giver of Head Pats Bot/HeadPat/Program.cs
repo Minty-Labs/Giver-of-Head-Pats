3cc158fad661bcdfdafd42e52f5109b1cef2f7b9
Lily <admin@mintlily.lgbt>
2022-07-12 13:57:43 -0400

    Updated Nuget packages

--- Giver of Head Pats Bot/HeadPat/Program.cs
+++ Giver of Head Pats Bot/HeadPat/Program.cs
@@ -26,8 +26,8 @@ public static class BuildInfo {
     public static readonly DateTime BuildTime = DateTime.Now;
     public static bool IsDebug = true;
 #elif !DEBUG
-    public const string Version = "4.3.1";
-    public static readonly DateTime BuildTime = new(2022, 7, 4, 23, 33, 00); // (year, month, day, hour, min, sec)
+    public const string Version = "4.3.2";
+    public static readonly DateTime BuildTime = new(2022, 7, 12, 13, 55, 00); // (year, month, day, hour, min, sec)
     public static bool IsDebug = false;
 #endif
     public static string BuildDateShort = $"{BuildTime.Day} {GetMonth(BuildTime.Month)} @ {BuildTime.Hour}:{ChangeSingleNumber(BuildTime.Minute)}";
