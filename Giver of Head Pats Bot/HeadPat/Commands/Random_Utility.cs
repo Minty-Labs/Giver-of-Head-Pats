6e4d4143d653d7de6da8e7a62b8d375025ec4376
Lily <admin@mintlily.lgbt>
2022-11-25 18:56:30 -0500

    Added color options: CMYK, HSV, & HSL

--- Giver of Head Pats Bot/HeadPat/Commands/Random_Utility.cs
+++ Giver of Head Pats Bot/HeadPat/Commands/Random_Utility.cs
@@ -12,36 +12,67 @@ public class Random_Utility : BaseCommandModule {
 
     [Command("Color"), Description("Shows you the color from the given input")]
     public async Task GiveColor(cc c,
-        [Description("Color type, RGB or or HEX")] string type,
-        [RemainingText, Description("Color value (no #, no rgb() | rgb separated by spaces or commas)")] string values = "") {
+        [Description("Color type, RGB, HEX, CMYK, HSV, HSL")] string type,
+        [RemainingText, Description("Color value (no #, no rgb() | non-HEX separated by spaces or commas)")] string values = "") {
         if (string.IsNullOrWhiteSpace(type)) {
-            await c.RespondAsync("Please specify a color type (`rgb` or `hex`)");
+            await c.RespondAsync("Please specify a color type\nTypes: `RGB, HEX, CMYK, HSV, HSL`");
             return;
         }
         if (string.IsNullOrWhiteSpace(values)) {
-            await c.RespondAsync("Please input a color value\nExamples: `fd3ac1` or `148, 78, 36` or `48 128 71`");
+            await c.RespondAsync("Please input a color value\nExamples: `fd3ac1` or `148, 78, 36` or `48 128 71` etc.");
             return;
         }
 
         const string baseUrl = "https://c.devminer.xyz/512/512";
         var em = new DiscordEmbedBuilder();
 
-        if (type.ToLower().Contains("rgb")) {
-            var tempRgb = values.ToLower().ReplaceAll("abcdefghijklmnopqrstuvwxyz()#!", "");
-            var hasCommas = tempRgb.Contains(',');
-            var newRgb = tempRgb.Split(hasCommas ? ',' : ' ');
-            var r = float.Parse(newRgb[0]);
-            var g = float.Parse(newRgb[1]);
-            var b = float.Parse(newRgb[2]);
-            em.WithColor(new DiscordColor(r / 255, g / 255, b / 255)); // DiscordColor is picky and wants decimal values *pukes in math*
-            em.WithImageUrl($"{baseUrl}/rgb/{r}/{g}/{b}");
-            em.WithTitle($"rgb({r}, {g}, {b})");
-        }
-        else {
-            var newHex = values.Replace("#", "").ToLower();
-            em.WithColor(Colors.HexToColor(newHex));
-            em.WithImageUrl($"{baseUrl}/hex/{newHex}");
-            em.WithTitle($"#{newHex}");
+        var tempColorValue = values.ToLower().ReplaceAll("abcdefghijklmnopqrstuvwxyz()#!", "");
+        var hasCommas = tempColorValue.Contains(',');
+        var newColorValue = tempColorValue.Split(hasCommas ? ',' : ' ');
+
+        switch (type) {
+            case "rgb": 
+                var r = float.Parse(newColorValue[0]);
+                var g = float.Parse(newColorValue[1]);
+                var b = float.Parse(newColorValue[2]);
+                em.WithColor(new DiscordColor(r / 255, g / 255, b / 255)); // DiscordColor is picky and wants decimal values *pukes in math*
+                em.WithImageUrl($"{baseUrl}/rgb/{r}/{g}/{b}");
+                em.WithTitle($"rgb({r}, {g}, {b})");
+                break;
+            case "hex": 
+                var newHex = values.Replace("#", "").ToLower();
+                em.WithColor(Colors.HexToColor(newHex));
+                em.WithImageUrl($"{baseUrl}/hex/{newHex}");
+                em.WithTitle($"#{newHex}");
+                break;
+            case "cmyk":
+                var _c = float.Parse(newColorValue[0]);
+                var m = float.Parse(newColorValue[1]);
+                var y = float.Parse(newColorValue[2]);
+                var k = float.Parse(newColorValue[3]);
+                var cmykToHex = ColorConverter.CmykToHex(new CMYK((byte)_c, (byte)m, (byte)y, (byte)k));
+                em.WithColor(Colors.HexToColor(cmykToHex.ToString()));
+                em.WithImageUrl($"{baseUrl}/hex/{cmykToHex}");
+                em.WithTitle($"#{cmykToHex}");
+                break;
+            case "hsv":
+                var h = float.Parse(newColorValue[0]);
+                var s = float.Parse(newColorValue[1]);
+                var v = float.Parse(newColorValue[2]);
+                var hsvToHex = ColorConverter.HsvToHex(new HSV((int)h, (byte)s, (byte)v));
+                em.WithColor(Colors.HexToColor(hsvToHex.ToString()));
+                em.WithImageUrl($"{baseUrl}/hex/{hsvToHex}");
+                em.WithTitle($"#{hsvToHex}");
+                break;
+            case "hsl":
+                var _h = float.Parse(newColorValue[0]);
+                var _s = float.Parse(newColorValue[1]);
+                var l = float.Parse(newColorValue[2]);
+                var hslToHex = ColorConverter.HslToHex(new HSL((int)_h, (byte)_s, (byte)l));
+                em.WithColor(Colors.HexToColor(hslToHex.ToString()));
+                em.WithImageUrl($"{baseUrl}/hex/{hslToHex}");
+                em.WithTitle($"#{hslToHex}");
+                break;
         }
         
         em.WithFooter("Powered by devminer.xyz");
