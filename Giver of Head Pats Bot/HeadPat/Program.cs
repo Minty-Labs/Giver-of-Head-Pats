0ce4778f22d547ebba5aed1dc9749eca71ec606d
Lily <admin@mintlily.lgbt>
2023-01-06 23:01:53 -0500

    version bump

--- Giver of Head Pats Bot/HeadPat/Program.cs
+++ Giver of Head Pats Bot/HeadPat/Program.cs
@@ -28,8 +28,8 @@ public static class BuildInfo {
     public static readonly DateTime BuildTime = DateTime.Now;
     public static bool IsDebug = true;
 #elif !DEBUG
-    public const string Version = "4.8.0";
-    public static readonly DateTime BuildTime = new(2022, 12, 28, 18, 42, 00); // (year, month, day, hour, min, sec)
+    public const string Version = "4.8.1";
+    public static readonly DateTime BuildTime = new(2023, 1, 6, 23, 00, 00); // (year, month, day, hour, min, sec)
     public static bool IsDebug = false;
 #endif
     public static string BuildDateShort = $"{BuildTime.Day} {GetMonth(BuildTime.Month)} @ {BuildTime.Hour}:{ChangeSingleNumber(BuildTime.Minute)}";
