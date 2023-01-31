da4fd55da0be432d81b8babe08c472b7698cf5cc
Lily <admin@mintlily.lgbt>
2023-01-30 19:56:13 -0500

    Created easy `message.DeleteAfter(time)` method

---
+++ Giver of Head Pats Bot/HeadPat/Managers/MessageTasks.cs
@@ -0,0 +1,10 @@
+﻿using DSharpPlus.Entities;
+
+namespace HeadPats.Managers; 
+
+public static class MessageTasks {
+    public static Task<DiscordMessage> DeleteAfter(this Task<DiscordMessage> message, int seconds) {
+        Task.Delay(TimeSpan.FromSeconds(seconds)).ContinueWith(async _ => await message.GetAwaiter().GetResult().DeleteAsync());
+        return Task.FromResult(message.GetAwaiter().GetResult());
+    }
+}
