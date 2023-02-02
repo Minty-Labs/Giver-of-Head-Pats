a8cdb169bc901a0b81651b531689439de373fbc1
Lily <admin@mintlily.lgbt>
2023-02-02 17:46:45 -0500

    Auto Delete old DM channels

---
+++ Giver of Head Pats Bot/HeadPat/Managers/AutoRemoveOldDmChannels.cs
@@ -0,0 +1,16 @@
+﻿using HeadPats.Handlers.Events;
+
+namespace HeadPats.Managers; 
+
+public static class AutoRemoveOldDmChannels {
+    public static async Task RemoveOldDmChannelsTask() {
+        foreach (var channel in MessageCreated.DmCategory!.Children) {
+            //if (channel.GuildId == BuildInfo.Config.SupportGuildId) continue;
+            if (channel.IsCategory) continue;
+            var messages = await channel.GetMessagesAsync(5);
+            var timeSpan = messages[0].CreationTimestamp.Subtract(DateTime.Now);
+            if (timeSpan.TotalSeconds >= 5259600) // 2 months
+                channel.DeleteAsync().GetAwaiter().GetResult();
+        }
+    }
+}
