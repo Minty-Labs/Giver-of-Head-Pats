09e6869903c6063a85d614536ec787a67db789b9
Lily <admin@mintlily.lgbt>
2023-02-24 00:26:42 -0500

    Added 🥗 and updated the cutie's name

--- Giver of Head Pats Bot/HeadPat/Managers/NameReplacing.cs
+++ Giver of Head Pats Bot/HeadPat/Managers/NameReplacing.cs
@@ -5,11 +5,12 @@ public static class NameReplacing {
         input = input.Replace("MintLily#0001", "Lily");
         input = input.Replace("Silentt.#5610", "Elly");
         input = input.Replace("Penny#9538", "Penny");
-        input = input.Replace(".FS.#8519", "Autumn");
+        input = input.Replace("AutumnFS#0540", "Autumn");
         input = input.Replace("Nail#3021", "Iana");
         input = input.Replace("Nail#9290", "Iana");
         input = input.Replace("jettsd#1111", "Emily");
         input = input.Replace("AxolotlFren#9690", "Erin");
+        input = input.Replace("Arshy, The Pie#1337", "🥗");
         return input;
     }
     
@@ -17,10 +18,11 @@ public static class NameReplacing {
         input = input.Replace("MintLily", "Lily");
         input = input.Replace("Silentt.", "Elly");
         input = input.Replace("Penny", "Penny");
-        input = input.Replace(".FS.", "Autumn");
+        input = input.Replace("AutumnFS", "Autumn");
         input = input.Replace("Nail", "Iana");
         input = input.Replace("jettsd", "Emily");
         input = input.Replace("AxolotlFren", "Erin");
+        input = input.Replace("Arshy, The Pie", "🥗");
         return input;
     }
 }
