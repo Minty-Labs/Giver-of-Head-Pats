4409f962c69f2236d1ff9f93f9b1adcd233ae6d6
Lily <admin@mintlily.lgbt>
2023-05-05 19:35:39 -0400

    fixed NameReplacing

--- Giver of Head Pats Bot/HeadPat/Managers/NameReplacing.cs
+++ Giver of Head Pats Bot/HeadPat/Managers/NameReplacing.cs
@@ -5,7 +5,7 @@ namespace HeadPats.Managers;
 
 public static class NameReplacing {
     public static string? ReplaceName(this string? beforeName, ulong userId) {
-        var d = Config.Base.NameReplacements!.FirstOrDefault(u => u.Equals(userId));
+        var d = Config.Base.NameReplacements!.Find(u => u.UserId == userId);
         return d is null ? beforeName : d.Replacement;
     }
 }
