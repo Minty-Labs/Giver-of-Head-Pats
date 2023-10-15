18803412fae7612230c8df404beb4ecf7419850e
Lily <admin@mintlily.lgbt>
2023-10-15 11:59:59 -0400

    Added things to the help command

--- Giver of Head Pats Bot/HeadPat/Managers/HelpFormatter.cs
+++ Giver of Head Pats Bot/HeadPat/Managers/HelpFormatter.cs
@@ -16,6 +16,11 @@ public class HelpFormatter : DefaultHelpFormatter {
         EmbedBuilder.AddField("Addition Information", 
             "There are also slash commands available for this bot. " +
             $"Don't see them? Keep the bot in your server and [re-invite it with this link]({Vars.InviteLink}) to update it's permissions.");
+        EmbedBuilder.AddField("Support Server", $"[Join the support server]({Vars.SupportServer}) for help and updates.");
+        EmbedBuilder.AddField("Other Links",
+            "[Privacy Policy](https://mintylabs.dev/gohp/privacy-policy)\n" +
+            "[Terms of Service](https://mintylabs.dev/gohp/terms)\n" +
+            "[Open Source Code](https://github.com/Minty-Labs/Giver-of-Head-Pats)");
         return base.Build();
     }
 }
