5a4b107b1438f487dd116e28538a660d76346d45
Lily <admin@mintlily.lgbt>
2023-12-06 19:52:56 -0500

    Add summary

--- Giver of Head Pats Bot/HeadPat.NET/Utils/TimeUtils.cs
+++ Giver of Head Pats Bot/HeadPat.NET/Utils/TimeUtils.cs
@@ -16,6 +16,11 @@ public static class TimeConverter {
     /// <returns>integer of seconds</returns>
     public static int GetSecondsFromUtcUnixTime(this DateTimeOffset dateTimeOffset) => (int)dateTimeOffset.DateTime.Subtract(new DateTime(1970, 1, 1, 0, 0, 0)).TotalSeconds;
     
+    /// <summary>
+    /// Converts a unix timestamp to a DateTime
+    /// </summary>
+    /// <param name="unixTimeStamp">long</param>
+    /// <returns>DateTime from unix timestamp</returns>
     public static DateTime UnixTimeStampToDateTime(this long unixTimeStamp) {
         var dateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
         dateTime = dateTime.AddSeconds(unixTimeStamp).ToLocalTime();
