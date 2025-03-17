213982527fe3c4699cbde26d6d4acfae7564ebe0
Lily <admin@mintlily.lgbt>
2025-03-17 19:51:30 -0400

    Fixed Cookie Leaderboard to guild member only output

--- Giver of Head Pats Bot/HeadPat.NET/Commands/Slash/UserLove/Leaderboards/TopCookie.cs
+++ Giver of Head Pats Bot/HeadPat.NET/Commands/Slash/UserLove/Leaderboards/TopCookie.cs
@@ -4,6 +4,7 @@ using Discord.Interactions;
 using HeadPats.Data;
 using HeadPats.Managers;
 using HeadPats.Utils;
+using Serilog;
 
 namespace HeadPats.Commands.Slash.UserLove.Leaderboards; 
 
@@ -12,14 +13,24 @@ public class TopCookie : InteractionModuleBase<SocketInteractionContext> {
     [SlashCommand("topcookie", "Get the Guild's top cookie leaderboard")]
     public async Task CookieLeaderboard([Summary("keywords", "Key words")] string keyWords = "") {
         await using var db = new Context();
-        var newUserList = db.Users.AsQueryable().ToList().OrderBy(p => -p.CookieCount);
+        var logger = Log.ForContext("SourceContext", "Command - TopCookie");
+
+        var globalCookie = 0;
+        try { globalCookie = db.Overall.AsQueryable().ToList().First().PatCount; }
+        catch { logger.Error("DataSet is Empty"); }
+        
+        
+        var newUserList = db.Users.AsQueryable().ToList().OrderBy(p => -p.PatCount);
+        var guildUserList = Context.Guild.Users.ToDictionary(user => user.Id);
+        
+        
         if (keyWords!.ToLower().Equals("server")) {
             var strings = new StringBuilder();
             strings.AppendLine("Top 50 that are in this server.");
             var counter = 1;
             foreach (var u in newUserList) {
                 if (counter >= 51) continue;
-                if (Context.Guild.Users.Any(m => m.Id != u.UserId)) continue;
+                if (!guildUserList.ContainsKey(u.UserId)) continue;
                 strings.AppendLine($"`{counter}.` {(u.UsernameWithNumber.Contains('#') ? u.UsernameWithNumber.Split('#')[0].ReplaceName(u.UserId) : u.UsernameWithNumber.ReplaceName(u.UserId))} - Total Cookies: {MarkdownUtils.ToBold(u.CookieCount.ToString("N0"))}");
                 counter++;
             }
@@ -33,7 +44,7 @@ public class TopCookie : InteractionModuleBase<SocketInteractionContext> {
 
         foreach (var u in newUserList) {
             if (max >= 11) continue;
-            if (Context.Guild.Users.Any(m => m.Id == u.UserId)) continue;
+            if (!guildUserList.ContainsKey(u.UserId)) continue;
             sb.AppendLine($"`{max}.` {(u.UsernameWithNumber.Contains('#') ? u.UsernameWithNumber.Split('#')[0].ReplaceName(u.UserId) : u.UsernameWithNumber.ReplaceName(u.UserId))} - Total Cookies: {MarkdownUtils.ToBold(u.CookieCount.ToString("N0"))}");
             max++;
         }
